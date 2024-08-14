64b2497d4bcffd3c8c47b05bd064c4b75b90dc5a
Larissa <larissa.rfs28@gmai.com>
2024-08-14 20:17:06 -0300

    Adicionando divisão inteira //

--- aulas-codigos/aula02_aritimetica.py
+++ aulas-codigos/aula02_aritimetica.py
@@ -1,15 +1,18 @@
 x = 14
 
-y = 2
+y = 3
 
 soma = x + y
 subtracao = x - y
 multiplicacao = x * y
 divisao = x / y
+divisao_inteira = x // y # divisão com resultado inteiro
+#o // indica que o resultado será inteiro
 
 
 print("A soma entre x e y é igual a ", soma)
 print("A subtração entre x e y é igual a ", subtracao)
 print("A multiplicação entre x e y é igual a ", multiplicacao)
 print("A divisão entre x e y é igual a ", divisao)
+print("A divisão inteira entre x e y é igual a ", divisao_inteira)
 
