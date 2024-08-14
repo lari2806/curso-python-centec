b30a7e74657486dfd3ba128f5c6e972bbebb205b
Larissa <larissa.rfs28@gmai.com>
2024-08-14 19:25:42 -0300

    Novo arquivo sobre Operações aritiméticas no python.

---
+++ aulas-codigos/aula02_aritimetica.py
@@ -0,0 +1,15 @@
+x = 14
+
+y = 2
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+
+
+print("A soma entre x e y é igual a ", soma)
+print("A subtração entre x e y é igual a ", subtracao)
+print("A multiplicação entre x e y é igual a ", multiplicacao)
+print("A divisão entre x e y é igual a ", divisao)
+
