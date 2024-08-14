f499d9930e575652e6e59ca24ff0ce1a35290638
Larissa <larissa.rfs28@gmai.com>
2024-08-14 20:42:59 -0300

    Adicionando potenciação ao código.

--- aulas-codigos/aula02_aritimetica.py
+++ aulas-codigos/aula02_aritimetica.py
@@ -6,8 +6,14 @@ soma = x + y
 subtracao = x - y
 multiplicacao = x * y
 divisao = x / y
-divisao_inteira = x // y # divisão com resultado inteiro
-#o // indica que o resultado será inteiro
+ # divisão com resultado inteiro
+divisao_inteira = x // y #o // indica que o resultado será inteiro
+
+#módulo
+resto = 14 % 6
+
+#potenciação
+potenciacao = 3 ** 5 
 
 
 print("A soma entre x e y é igual a ", soma)
@@ -15,4 +21,5 @@ print("A subtração entre x e y é igual a ", subtracao)
 print("A multiplicação entre x e y é igual a ", multiplicacao)
 print("A divisão entre x e y é igual a ", divisao)
 print("A divisão inteira entre x e y é igual a ", divisao_inteira)
-
+print("o resultado deste módulo é  ", resto)
+print("o resultado desta potenciação é ", potenciacao)
