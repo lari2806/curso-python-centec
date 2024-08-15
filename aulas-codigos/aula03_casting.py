f07ec90dfd1634c4558ad30bb301b9dbe497faf3
Larissa <larissa.rfs28@gmai.com>
2024-08-15 19:21:53 -0300

    Adicioando ao arquivo como Tranformar String em Inteiros.

--- aulas-codigos/aula03_casting.py
+++ aulas-codigos/aula03_casting.py
@@ -1,12 +1,28 @@
-texto = "3.30"
-valor = "7.75"
+#casting é a conversãod de tipos de dados, manipula as informções mudando o tipo da variável.
 
+#Exemplos de Casting.
+
+texto = "3.30"
+texto2 = "7.75"
+
+#transformando as Strings em floats.
 tranformacao_numero1 = float(texto)
 
 print(5 + tranformacao_numero1)
 
-tranformacao_numero2 = float(texto)
+tranformacao_numero2 = float(texto2)
 
 print(5 + tranformacao_numero2)
 
 print(tranformacao_numero1 + tranformacao_numero2)
+
+#Transformar em inteiro
+
+texto3 = "3"
+texto4 = "7"
+
+#transformando as Strings em inteiros.
+tranformacao_inteiro1 = int(texto3)
+transformacao_inteiro2 = int(texto4)
+
+print(tranformacao_inteiro1 + transformacao_inteiro2)
