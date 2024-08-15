4535fdcf1435895a8de30c44fa003f35b7c853a4
Larissa <larissa.rfs28@gmai.com>
2024-08-15 19:01:56 -0300

    Tranformando tipos String em Float.

---
+++ aulas-codigos/aula03_casting.py
@@ -0,0 +1,12 @@
+texto = "3.30"
+valor = "7.75"
+
+tranformacao_numero1 = float(texto)
+
+print(5 + tranformacao_numero1)
+
+tranformacao_numero2 = float(texto)
+
+print(5 + tranformacao_numero2)
+
+print(tranformacao_numero1 + tranformacao_numero2)
