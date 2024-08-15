fb8d7004f8af19d9389da990868a4f3a28a0876b
Larissa <larissa.rfs28@gmai.com>
2024-08-15 19:00:46 -0300

    Novo arquivo  sobre o que foi dito na aula 01.

---
+++ aulas-codigos/aula01.py
@@ -0,0 +1,9 @@
+print('olá mundo')
+
+# tipos de dados primitivos
+# INTEIRO: ... -4 -3 -2 -1 0 1 2 3 4 5 29...
+# FLOAT: 3.14 10.0 16.32 32.50
+# STRING: alfanumérico (abcdefgh...z 0-9.,!?)
+#       Ex.: 'Davi', 'ABC', 'Artur'
+#        'O rato roeu a roupa do rei de roma!'
+#        'Minha idade é: 29!'
