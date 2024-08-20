6cc58e5d4ca0c4da0b27c8779ee0385fb488c348
Larissa <larissa.rfs28@gmai.com>
2024-08-19 21:00:36 -0300

    Adicionando novo arquivo sobre curso.

---
+++ aulas-codigos/aula05_laco_for.py
@@ -0,0 +1,11 @@
+# ESTRUTURA FOR:
+#inicio: 1
+#fim:15
+#passo: 2 (+2 +2 +2)
+
+# FUNÇÃO RANGE()
+#inicio: 1 (inclusivo) -> inclui
+#fim: 10 (exclusivo) -> exclui o ultimo valor, valor 10
+
+for valor in range(1,10,1):
+    print(valor)
