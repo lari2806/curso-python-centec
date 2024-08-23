36a7a34d54f8a9b55be4d2218960b8a422b2c095
Larissa <larissa.rfs28@gmai.com>
2024-08-23 18:18:33 -0300

    novo arquivo adicionado

---
+++ desafios-python/q2_numeros_pares.py
@@ -0,0 +1,8 @@
+pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#for par in range(len(pares)):
+for par in pares:
+#   if pares[par] % 2 == 0:
+    if par % 2 == 0:
+#       print(pares[par])        
+        print(par)
