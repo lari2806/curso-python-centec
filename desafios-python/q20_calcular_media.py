12b0b620ac71d9eb31d05ec22ebbf6093ee7bed1
Larissa <larissa.rfs28@gmai.com>
2024-08-28 19:15:09 -0300

    modificando arquivo.

--- desafios-python/q20_calcular_media.py
+++ desafios-python/q20_calcular_media.py
@@ -1,13 +1,13 @@
 quantidade_numeros = int(input('Digite a quant. de números: '))
 
 
-somatorio = 0
+soma = 0
 
 
 for x in range(quantidade_numeros):
     valor = float(input(f'Digite o {x + 1}º valor: '))
 
 
-    somatorio += valor
+    soma += valor
 
-print('Média:', somatorio / quantidade_numeros)
+print('Média:', soma / quantidade_numeros)
