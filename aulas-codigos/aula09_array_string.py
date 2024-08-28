310833232a0a32e6bdc0e86c7c552bfac2e7089e
Larissa <larissa.rfs28@gmai.com>
2024-08-28 19:10:38 -0300

    Adicionando novo arquivo.

---
+++ aulas-codigos/aula09_array_string.py
@@ -0,0 +1,5 @@
+lista_numeros = [2,5,8,9]
+
+for indice in range(4 ):
+    valor = lista_numeros[indice]
+    print(valor)
---
+++ desafios-python/q20_calcular_media.py
@@ -0,0 +1,4 @@
+quantidade_numeros = int(input("Digite a quantidade de numeros: "))
+
+for i in range(quantidade_numeros):
+    valor = float
