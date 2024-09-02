c8e76a7311b81fa32e3103bb160a5263eb1f6eff
Larissa <larissa.rfs28@gmai.com>
2024-09-02 18:42:50 -0300

    novo arquivo.

---
+++ aulas-codigos/aula12_dictionary_v2.py
@@ -0,0 +1,27 @@
+lista = ["Thomaz", "Larissa", "Laura", "Savio"]
+
+print(lista)
+
+print(lista[2])
+
+dicionario ={
+    "primeiro_usuario": {
+        "nome" : "Thomaz",
+        "idade" : 9      
+                         } ,
+    "segundo_usuario":
+        {"nome":"Larissa",
+         "idade" : 18
+         },
+
+    "terceiro_usuario" :{
+         "nome" : "Laura",
+         "idade" : 4
+    }
+}
+cadastro_pessoa1 ={
+    "nome": "Thomaz",
+    "idade": 9,
+    
+}
+print(dicionario)
